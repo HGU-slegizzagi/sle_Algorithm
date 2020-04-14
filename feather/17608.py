93244cc2268311198732dfa3b3e311c69f0a40e5
GiCheol.Kim <37562319+gitcheol@users.noreply.github.com>
2020-04-14 12:42:34 +0900

    Create 17608.py
    Input speed is important factor of the python3

---
+++ feather/17608.py
@@ -0,0 +1,14 @@
+import sys 
+
+N=int(input())
+L=[]
+for i in range(N):
+    L.append(int(sys.stdin.readline()))
+
+count=1
+tmp=L[N-1]
+for i in range(N-1,0,-1):
+    if L[i-1] > tmp:
+        count+=1
+        tmp=L[i-1]
+print(count)
