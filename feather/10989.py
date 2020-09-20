676293fe7ad470235d4ca4009369174e0da39087
GiCheol.Kim <37562319+gitcheol@users.noreply.github.com>
2020-09-21 00:35:29 +0900

    Update 10989.py

--- feather/10989.py
+++ feather/10989.py
@@ -1,17 +1,10 @@
-import sys 
-
+import sys
 N=int(sys.stdin.readline())
 s=[0]*10001
 
 for i in range(N):
     s[int(sys.stdin.readline())]+=1
- 
-idx=1
-while True:
-    if idx==10001:
-        break
-    if s[idx]==0:
-        idx+=1
-        continue
+    
+for idx in range(1,10001):
     print((str(idx)+'\n')*s[idx],end='')
-    idx+=1
+ 
